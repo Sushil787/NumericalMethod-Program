87860534405ecbb1c6dbc97ab78d8e53f277e289
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2022-01-18 20:13:16 -0800

    Create trapzodalRule.c

---
+++ trapzodalRule.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+/* Define function here */
+#define f(x) 1/(1+pow(x,2))
+
+int main()
+{
+ float lower, upper, integration=0.0, stepSize, k;
+ int i, subInterval;
+ clrscr();
+ /* Input */
+ printf("Enter lower limit of integration: ");
+ scanf("%f", &lower);
+ printf("Enter upper limit of integration: ");
+ scanf("%f", &upper);
+ printf("Enter number of sub intervals: ");
+ scanf("%d", &subInterval);
+
+ /* Calculation */
+ /* Finding step size */
+ stepSize = (upper - lower)/subInterval;
+
+ /* Finding Integration Value */
+ integration = f(lower) + f(upper);
+ for(i=1; i<= subInterval-1; i++)
+ {
+  k = lower + i*stepSize;
+  integration = integration + 2 * f(k);
+ }
+ integration = integration * stepSize/2;
+ printf("\nRequired value of integration is: %.3f", integration);
+ getch();
+ return 0;
+}
